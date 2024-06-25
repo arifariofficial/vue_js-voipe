3b0a454e337fa72001f435b7413d551977b1a482
Ariful Islam <ariful6@yahoo.com>
2024-06-26 02:44:00 +0300

    lesson 16

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,10 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <div id="app">
+    <h1>To-Do Lista</h1>
+    <ul>
+      <li v-for="item in TodoItems" :key="item.id">
+        <todo-item :label="item.label" :done="item.done"></todo-item>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+  import TodoItem from "./components/ToDoItem.vue";
+  import uniqueId from "lodash.uniqueid";
+
   export default {
     name: "App",
+    components: {
+      TodoItem,
+    },
+    data() {
+      return {
+        TodoItems: [
+          { id: uniqueId("todo-"), label: "Opiskele Vue.js", done: false },
+          { id: uniqueId("todo-"), label: "Tee eka vue.js app", done: true },
+          { id: uniqueId("todo-"), label: "Pida kivaaa", done: true },
+          { id: uniqueId("todo-"), label: "Tee to-do lista", done: false },
+          { id: uniqueId("todo-"), label: "Ota opntopisteet kotiin", done: false },
+        ],
+      };
+    },
   };
 </script>
 
