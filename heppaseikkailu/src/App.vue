9d768a778eeb3cac1232fb154723906030dca352
Ariful Islam <ariful6@yahoo.com>
2024-06-26 06:59:52 +0300

    lesson36

---
+++ heppaseikkailu/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="container" style="margin-top: 70px">
+    <h1>Poniralli</h1>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {}
+});
+</script>
+<style></style>
