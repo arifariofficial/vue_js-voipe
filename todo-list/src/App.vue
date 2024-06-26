49a7f784bebce8875b1d1be84b52720006ad49b5
Ariful Islam <ariful6@yahoo.com>
2024-06-26 06:00:29 +0300

    lesson 34

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -2,9 +2,15 @@
   <div id="app">
     <h1>To-Do Lista</h1>
     <todo-form @todo-added="addToDo"></todo-form>
-    <ul>
+    <h2 id="list-summery" ref="listSummary" tabindex="-1">{{ listSummary }}</h2>
+    <ul class="stack-large">
       <li v-for="item in TodoItems" :key="item.id">
-        <todo-item :label="item.label" :done="item.done" :id="item.id"></todo-item>
+        <todo-item
+          :label="item.label"
+          :done="item.done"
+          :id="item.id"
+          @checkbox-changed="($event) => updateDoneStatus(item.id)"
+        ></todo-item>
       </li>
     </ul>
   </div>
@@ -36,17 +42,56 @@
       addToDo(toDoLabel) {
         this.TodoItems.push({ id: uniqueId("todo-"), label: toDoLabel, done: false });
       },
+      updateDoneStatus(toDoId) {
+        const todDoUpdate = this.TodoItems.find((item) => item.id === toDoId);
+        todDoUpdate.done = !todDoUpdate.done;
+      },
+    },
+    computed: {
+      listSummary() {
+        const numberFinishedItems = this.TodoItems.filter((item) => item.done).length;
+        return `${numberFinishedItems} out of ${this.TodoItems.length} items completed`;
+      },
     },
   };
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+  .stack-small > * + * {
+    margin-top: 1.25rem;
+  }
+  .stack-large > * + * {
+    margin-top: 2.5rem;
+  }
+  @media screen and (min-width: 550px) {
+    .stack-small > * + * {
+      margin-top: 1.4rem;
+    }
+    .stack-large > * + * {
+      margin-top: 2.8rem;
+    }
+  }
+  /* End global styles */
+
+  @media screen and (min-width: 550px) {
+    #app {
+      padding: 4rem;
+    }
+  }
+  #app > * {
+    max-width: 50rem;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  #app > form {
+    max-width: 100%;
+  }
+  #app h1 {
+    display: block;
+    min-width: 100%;
+    width: 100%;
     text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+    margin: 0;
+    margin-bottom: 1rem;
   }
 </style>
--- todo-list/src/components/ToDoForm.vue
+++ todo-list/src/components/ToDoForm.vue
@@ -1,14 +1,17 @@
 <template>
   <form @submit.prevent="onSubmit">
-    <label for="new-todo-input">Mitä tänään pitäisi tehdä? </label>
+    <h2 class="label-wrapper">
+      <label for="new-todo-input" class="label_lg">Mitä tänään pitäisi tehdä? </label>
+    </h2>
     <input
       type="text"
       name="new-todo"
       id="new-todo-input"
       autocomplete="off"
       v-model.lazy.trim="label"
+      class="input_lg"
     />
-    <button type="submit">Lisää</button>
+    <button type="submit" class="btn btn_primary btn_lg">Lisää</button>
   </form>
 </template>
 
@@ -30,3 +33,19 @@
     },
   };
 </script>
+
+<style scoped>
+  .btn {
+    margin-left: 10px;
+    border: 1px solid black;
+    border-radius: 5px;
+    padding: 10px;
+    background-color: white;
+    color: black;
+    font-size: 16px;
+    font-weight: bold;
+    cursor: pointer;
+    outline: none;
+    transition: all 0.2s ease-in-out;
+  }
+</style>
